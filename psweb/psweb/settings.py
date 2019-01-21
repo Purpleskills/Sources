50a62e2f7773ad1cc9ec5d2d17a7bc658d35c8d9
barna_am <barna@assessmachine.com>
2019-01-21 23:11:53 +0530

    separated provider specific schemas and added helpdoc

--- psweb/psweb/settings.py
+++ psweb/psweb/settings.py
@@ -61,7 +61,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'psweb',
     'home',
-    'learn'
+    'learn',
+    'contentprovider'
 )
 
 MIDDLEWARE = (
@@ -104,21 +105,7 @@ LOGOUT_REDIRECT_URL = '/home/'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 if 'RDS_HOSTNAME' not in os.environ:
- #   DATABASES = {
- #       'default': {
- #           'ENGINE': 'django.db.backends.mysql',
- #           'NAME': 'purpleskillsdb',
- #           'USER': 'psroot',
- #           'PASSWORD': 'pswhatever1',
- #           'HOST': '127.0.0.1',
- #           'PORT': '',
- #           'OPTIONS': {
- #                   'charset': 'utf8mb4',
- #                   'use_unicode': True, },
- #           }
- #       }
-   
-   
+
     DATABASES = {
     	'default': {
         	'ENGINE': 'django.db.backends.postgresql_psycopg2',
